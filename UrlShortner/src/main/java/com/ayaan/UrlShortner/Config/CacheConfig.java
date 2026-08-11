fddaad75bf7afdad00b9a1ce900d0d85fc0a9080
Ayaan Pathan <ayaanpathanra@gmail.com>
2026-08-11 15:25:29 +0530

    Feature/Add TTL for cache

---
+++ UrlShortner/src/main/java/com/ayaan/UrlShortner/Config/CacheConfig.java
@@ -0,0 +1,31 @@
+package com.ayaan.UrlShortner.Config;
+
+import org.springframework.boot.cache.autoconfigure.RedisCacheManagerBuilderCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.serializer.GenericJacksonJsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+
+import java.time.Duration;
+
+@Configuration
+public class CacheConfig {
+
+    @Bean
+    public RedisCacheManagerBuilderCustomizer redisCacheManagerBuilderCustomizer() {
+
+        return builder -> builder
+                .withCacheConfiguration(
+                        "urlCache",
+                        RedisCacheConfiguration.defaultCacheConfig()
+                                .entryTtl(Duration.ofHours(1))
+                                .serializeValuesWith(
+                                        RedisSerializationContext.SerializationPair
+                                                .fromSerializer(
+                                                        new GenericJacksonJsonRedisSerializer()
+                                                )
+                                )
+                );
+    }
+}
